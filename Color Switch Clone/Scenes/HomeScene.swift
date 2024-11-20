33b1976c39ebf59dbca2d9594a0a264a3b152b29
seedy1 <jobe.seedy@gmail.com>
2024-11-20 02:03:04 +0100

    added game logic and new screen

---
+++ Color Switch Clone/Scenes/HomeScene.swift
@@ -0,0 +1,12 @@
+//
+//  HomeScene.swift
+//  Color Switch Clone
+//
+//  Created by Seedy on 20/11/2024.
+//
+
+import UIKit
+
+class HomeScene: SKScene {
+
+}
---
+++ Color Switch Clone/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  Color Switch Clone
+//
+//  Created by Seedy on 12/11/2024.
+//
+
+import Foundation
